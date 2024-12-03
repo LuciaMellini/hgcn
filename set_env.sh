9f8a60d908b1d685afe3e757d3ea405491b202ad
LuciaMellini <luciaanna.mellini@studenti.unimi.it>
2024-12-03 16:01:31 +0100

    ignore logs

--- set_env.sh
+++ set_env.sh
@@ -4,4 +4,4 @@ export LOG_DIR="$HGCN_HOME/logs"
 export PYTHONPATH="$HGCN_HOME:$PYTHONPATH"
 export DATAPATH="$HGCN_HOME/data"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64
-source hgcn/bin/activate  # replace with  if you used a virtualenv source activate hgcn
+source hgcn/bin/activate  # replace with if you used anaconda source activate hgcn
